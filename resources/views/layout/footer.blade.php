20968f9fe4c287ad89113f4783fee32f1c45de0c
MuhamDaily <muhampedia.id@gmail.com>
2025-12-22 22:16:11 +0700

    Add team members modal to footer for improved accessibility

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,9 +2,35 @@
     <div class="page-container">
         <div class="row">
             <div class="col-md-12 text-center text-md-start">
-                Copyright &copy; {{ date('Y') }} <span
-                    class="fw-bold text-decoration-underline text-uppercase text-reset fs-12">{{ config('app.name') }}</span>.
+                Copyright &copy; {{ date('Y') }}
+                <span class="fw-bold text-decoration-underline text-uppercase text-reset fs-12" style="cursor: pointer;"
+                    data-bs-toggle="modal" data-bs-target="#anggotaModal">
+                    {{ config('app.name') }}
+                </span>.
             </div>
         </div>
     </div>
 </footer>
+
+<div class="modal fade" id="anggotaModal" tabindex="-1" aria-labelledby="anggotaModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="anggotaModalLabel">
+                    Daftar Anggota Tim Pengembang {{ config('app.name') }}
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <ul class="list-group">
+                    <li class="list-group-item"><b>41522010153</b> - ATILLA KUNCORO DJATI (Backend)</li>
+                    <li class="list-group-item"><b>41522010150</b> - NAUFAL ATHILLAH (Frontend)</li>
+                    <li class="list-group-item"><b>41522010061</b> - RAFLY PRIATMOJO (UI/UX)</li>
+                </ul>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-soft-dark shadow-sm" data-bs-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
